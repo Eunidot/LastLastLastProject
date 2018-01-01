50c62023a84178e274d085d355eb4b1948abbef1
eun <hanje0225@gmail.com>
2018-01-01 22:19:28 +0900

    Main class

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+    	MyActionListener my = new MyActionListener();
+    	my.LoginListenerSet();
+    	AppManager.getInstance().getLoginView();
+    }
+}
+
